a513639849d323f12c6dde4d6cfca8b74b6c5d79
daikimiura <daikiii5555@gmail.com>
2020-10-20 01:39:36 +0900

    Implement a kernel page table per process

--- kernel/defs.h
+++ kernel/defs.h
@@ -164,6 +164,8 @@ uint64          kvmpa(uint64);
 void            kvmmap(uint64, uint64, uint64, int);
 int             mappages(pagetable_t, uint64, uint64, uint64, int);
 pagetable_t     uvmcreate(void);
+pagetable_t     kvmcreate(void);
+void            kvmfree(pagetable_t);
 void            uvminit(pagetable_t, uchar *, uint);
 uint64          uvmalloc(pagetable_t, uint64, uint64);
 uint64          uvmdealloc(pagetable_t, uint64, uint64);
@@ -178,6 +180,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+void            kvmswitch_kernel(void);
+void            kvmswitch(pagetable_t);
 
 // plic.c
 void            plicinit(void);
