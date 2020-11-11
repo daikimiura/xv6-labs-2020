73311501854f06fa13d6ed74a65db9f1930a3ec3
daikimiura <daikiii5555@gmail.com>
2020-11-12 02:38:55 +0900

    Modify put() operations run in parallel while maintaining correctness

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t put_lock[NBUCKET];
+
 struct entry {
   int key;
   int value;
@@ -42,6 +44,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  pthread_mutex_lock(&put_lock[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -53,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&put_lock[i]);
 }
 
 static struct entry*
@@ -102,6 +106,8 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+  for (int i = 0; i < NBUCKET;i++)
+    pthread_mutex_init(&put_lock[i], NULL);
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
